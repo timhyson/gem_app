6c661ee7a52f8ca1448ae55d076583df4f214d95
Tim Hyson <tjhyson@gmail.com>
2015-10-27 12:22:19 +0000

    allow users to access sign-in page from homepage

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
 
+  devise_for :users
 resources :addresses
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-root 'welcome#index'
+root to: 'addresses#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
